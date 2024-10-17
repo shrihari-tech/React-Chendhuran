832096784876fa05860e8b3c4794530e6055ed52
shrihari-tech <bhshrihari@gmail.com>
2024-10-17 10:45:01 +0530

    usenavigate

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,19 @@
 import React from "react";
 import PropTypes from 'prop-types';
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 const Home = ({first,last}) => {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate("/About");
+    } 
     return (
         <div id="/">
         <h1>First Name {first}</h1>
         <h1>Last Name {last}</h1>
+
+        <button onClick={handleClick}>About</button>
+
         <Link to="/About">About</Link> &
         <Link to="/state">State  </Link>
         </div>
